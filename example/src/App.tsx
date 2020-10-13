260b3e9cd07624170da380abe36a2c1c7f1a796f
amerllica <amerllica@gmail.com>
2020-10-13 13:18:52 +0330

    Fix packages to reach a native error

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import IrPlayer from 'react-native-ir-player';
 
 export default function App() {
-
   return (
     <View style={styles.container}>
       <IrPlayer />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { requireNativeComponent, NativeModules } from 'react-native';
+import { requireNativeComponent } from 'react-native';
 
 const IrPlayerView = requireNativeComponent('RCTIrPlayer');
 
 export default class IrPlayer extends React.PureComponent {
   render() {
-    return <IrPlayerView />
+    return <IrPlayerView />;
   }
 }
