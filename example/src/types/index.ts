767f5492efc2b70e8640e8dced8603d90bec583d
amerllica <amerllica@gmail.com>
2020-11-24 12:35:52 +0330

    improvement: create system type

---
+++ example/src/types/index.ts
@@ -0,0 +1,12 @@
+import type { StyleProp, ViewStyle } from 'react-native';
+import type { IrPlayerMethodsType, MediaInfo } from 'react-native-ir-player';
+// @ts-ignore
+import type { MaterialIconsGlyphs } from 'react-native-vector-icons/MaterialIcons.js.flow';
+
+export { IrPlayerMethodsType, MediaInfo };
+
+export type ViewStyleType = StyleProp<ViewStyle>;
+
+export type VoidFunction = (...args: any[]) => void;
+
+export type IconNamesType = MaterialIconsGlyphs;
