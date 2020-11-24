2a173256609787251ff64eb989ff25dddb7dd706
amerllica <amerllica@gmail.com>
2020-11-24 12:40:11 +0330

    improvement: rename the main example view

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './src/App';
+import VideoView from './src/VideoView';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => VideoView);
