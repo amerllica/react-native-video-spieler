8ce8707be6f60ac19f1da5b44fa4ae790e38c308
amerllica <amerllica@gmail.com>
2020-11-24 12:37:03 +0330

    improvement: create theme system

---
+++ example/src/helpers/theme.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: '#e50914',
+  white: '#ffffff',
+  black: '#000000',
+  gray: '#6a6a6a',
+  blackGlass: 'rgba(0,0,0,0.65)',
+};
+
+export const grids = {
+  unit: 8,
+  gutter: 16,
+};
+
+export const sizes = {
+  icon: 24,
+  iconButton: 40,
+};
